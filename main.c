305ad9415b93a4c6d7e82a6de6c379b18314f05c
marcosantos98 <marcomsantos98@gmail.com>
2026-05-11 10:59:30 +0100

    generate ir and run it

--- main.c
+++ main.c
@@ -688,6 +688,122 @@ void print_node(Node *node) {
     }
 }
 
+typedef enum {
+    OT_NONE,
+    OT_LABEL,
+    OT_CALL,
+    OT_PUSH_STR,
+} OpType;
+
+typedef struct {
+    OpType type;
+    int operand;
+} Op;
+
+typedef struct {
+    Op *items;
+    int capacity;
+    int count;
+} Ops;
+
+typedef struct {
+    String_View *items;
+    int capacity;
+    int count;
+} StringTable;
+
+void ir_generate_for_node(Node *node, Ops *ops, StringTable *strings) {
+    switch (node->type) {
+    case NT_FN_DECL: {
+        Op op = {
+            .type = OT_LABEL,
+            .operand = strings->count,
+        };
+        da_append(strings, node->fn_decl_stmt->name);
+        da_append(ops, op);
+        ir_generate_for_node(node->fn_decl_stmt->block, ops, strings);
+        // fixme(marco): generate end of function
+    } break;
+    case NT_BLOCK: {
+        for (int i = 0; i < node->block_stmt->nodes.count; i += 1) {
+            ir_generate_for_node(node->block_stmt->nodes.items[i], ops, strings);
+        }
+    } break;
+    case NT_EXPR_STMT: {
+        ir_generate_for_node(node->expr_stmt->expr, ops, strings);
+    } break;
+    case NT_LIT_STRING: {
+        Op op = {
+            .type = OT_PUSH_STR,
+            .operand = strings->count,
+        };
+        da_append(strings, node->lit_string_expr->lit);
+        da_append(ops, op);
+    } break;
+    case NT_IDENT_EXPR: {
+        printf("NT_IDENT_EXPR\n");
+    } break;
+    case NT_FN_CALL_EXPR: {
+        for (int i = 0; i < node->fn_call_expr->args.count; i += 1) {
+            ir_generate_for_node(node->fn_call_expr->args.items[i], ops, strings);
+        }
+        Op op = {
+            .type = OT_CALL,
+            .operand = strings->count,
+        };
+        da_append(strings, node->fn_call_expr->name);
+        da_append(ops, op);
+    } break;
+    }
+}
+
+bool ir_generate(Parser p) {
+    Ops ops = {0};
+    StringTable strings = {0};
+
+    for (int i = 0; i < p.nodes.count; i += 1) {
+        Node *node = p.nodes.items[i];
+        ir_generate_for_node(node, &ops, &strings);
+    }
+
+    for (int i = 0; i < ops.count; i += 1) {
+        Op op = ops.items[i];
+        printf("[%d] type:(%d) operand:(%d) as_string:(%.*s)\n", i, op.type, op.operand, SV_Arg(strings.items[op.operand]));
+    }
+
+    printf("Running IR:\n");
+
+    int stack[1024] = {0};
+    int sp = 1024;
+
+    for (int i = 0; i < ops.count; i += 1) {
+        Op op = ops.items[i];
+        switch (op.type) {
+        case OT_LABEL:
+            break;
+        case OT_CALL: {
+            String_View name = strings.items[op.operand];
+            if (sv_eq(name, sv_from_cstr("puts"))) {
+                int argop = stack[sp];
+                sp += 1;
+                String_View arg = strings.items[argop];
+                printf("%.*s\n", SV_Arg(arg));
+            } else {
+                printf("Find fncall by name\n");
+            }
+        } break;
+        case OT_PUSH_STR: {
+            stack[--sp] = op.operand;
+        } break;
+        case OT_NONE: {
+            return false;
+        }
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, char **argv) {
 
     if (argc < 2)
@@ -745,6 +861,11 @@ int main(int argc, char **argv) {
     for (int i = 0; i < parser.nodes.count; i += 1) {
         Node *node = parser.nodes.items[i];
         print_node(node);
+    }
+
+    if (!ir_generate(parser)) {
+        printf("Failed to generate ir\n");
+        return 1;
     }
 
     return 0;
